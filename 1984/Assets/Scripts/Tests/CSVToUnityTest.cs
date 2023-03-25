8a8b43d3558d202e8784d473191694624833f109
Nakuri <ccmuk58@naver.com>
2023-03-26 01:42:39 +0900

    Test: Log Dialogue,csv
    CSVToUnityTest.cs 추가
    
    JHScene CSV Read 로그 테스트

---
+++ 1984/Assets/Scripts/Tests/CSVToUnityTest.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+using Structs;
+using UnityEngine;
+
+public class CSVToUnityTest : MonoBehaviour
+{
+    public Dictionary<string, List<DialogueData>> dictionary;
+    private void Start()
+    {
+        var csvReader = new CSVReader();
+
+        dictionary = csvReader.Read("Assets/Documentation/Dialogue.csv");
+
+        foreach (var value in dictionary)
+        {
+            foreach (var list in value.Value)
+            {
+                Debug.Log(list.name + " : " + list.script);
+            }
+        }
+    }
+}
