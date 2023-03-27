b48fadb46e472b0ac504fac775ef744a2453b870
Nakuri <ccmuk58@naver.com>
2023-03-27 19:32:34 +0900

    Fix: Comma and Newline Exception
    콤마의 경우 모두 하나의 string으로 합친 후 병합
    
    개행의 경우 이전 줄과 합친 후 병합

--- 1984/Assets/Scripts/Runtime/Utils/CSVReader.cs
+++ 1984/Assets/Scripts/Runtime/Utils/CSVReader.cs
@@ -1,31 +1,62 @@
 ﻿using System.Collections.Generic;
 using System.IO;
-using Enums;
+using Const;
 using Structs;
+using UnityEngine;
 
 public class CSVReader
 {
-    
-    // Dialogue CSV Read
     public Dictionary<string, List<DialogueData>> Read(string filePath)
     {
-        StreamReader reader = new StreamReader(filePath);
+        var reader = new StreamReader(filePath);
         var dictionary = new Dictionary<string, List<DialogueData>>();
         
         // Header 제거
         reader.ReadLine();
         while (!reader.EndOfStream)
         {
-            string line = reader.ReadLine();
-            string[] values = line.Split(',');
-            
-            // key를 기준으로 name, script 저장
-            // key : Scnen + Branch, ex) 윈스턴의 방 + 1-2
-            string key = values[(int)DialogueHeader.Scene] + values[(int)DialogueHeader.Branch];
+            var cells = new List<string>();
+            bool isQuoteFront;
+            bool isQuoteEnd;
+
+            do
+            {
+                string line = reader.ReadLine();
+                if (line == null) break;
+                string[] splitLine = line.Split(",");
+                
+                // 콤마 예외 처리
+                if (splitLine.Length > Consts.SCRIPT)
+                {
+                    for (int i = Consts.SCRIPT + 1; i < splitLine.Length; i++)
+                    {
+                        splitLine[Consts.SCRIPT] += splitLine[i];
+                    }
+                }
+                
+                if (cells.Count == 0)
+                {
+                    for (int i = 0; i <= Consts.SCRIPT; i++)
+                    {
+                        cells.Add(splitLine[i]);
+                    }
+                }
+                else
+                    cells[^1] += '\n'+line;
+
+                // 줄바꿈 예외 처리
+                isQuoteFront = cells[^1][0] == '\"';
+                isQuoteEnd = cells[^1][^1] == '\"';
+            } while (isQuoteFront ^ isQuoteEnd);
+
+            // key : Scnen + Branch
+            string key = cells[Consts.SCENE] + cells[Consts.BRANCH];
             
             if (!dictionary.ContainsKey(key))
-                dictionary.Add(key, new List<DialogueData>()); 
-            var data = new DialogueData(values[(int)DialogueHeader.Name], values[(int)DialogueHeader.Script]);
+                dictionary.Add(key, new List<DialogueData>());
+            
+            // key를 기준으로 Name과 Script 저장
+            var data = new DialogueData(cells[Consts.NAME], cells[Consts.SCRIPT]);
             dictionary[key].Add(data);
         }
         reader.Close();
