dacb79b53adb3c84cfe1a95759dd458fcd68f00d
Nakuri <ccmuk58@naver.com>
2023-04-08 02:26:58 +0900

    Feat: Define Dialogue file path
    Define dialogue file path
    Remove CSV definition

--- 1984/Assets/Scripts/Runtime/Utils/Define.cs
+++ 1984/Assets/Scripts/Runtime/Utils/Define.cs
@@ -19,18 +19,6 @@ namespace Structs
         public int score;
         public int level;
     }
-
-    public struct DialogueData
-    {
-        public readonly string name;
-        public readonly string script;
-
-        public DialogueData(string name, string script)
-        {
-            this.name = name;
-            this.script = script;
-        }
-    }
 }
 
 namespace Const
@@ -41,9 +29,7 @@ namespace Const
         public const int MAX_SCORE = 100;
         public const int MAX_LEVEL = 10;
 
-        public const int SCENE = 0;
-        public const int BRANCH = 1;
-        public const int NAME = 2;
-        public const int SCRIPT = 3;
+        public const string DIALOGUE_EXCEL_PATH = "Assets/Resources/Dialogue/Dialogue.xlsx";
+        public const string DIALOGUE_JSON_PATH = "Assets/Resources/Dialogue/DialogueJson.json";
     }
 }
