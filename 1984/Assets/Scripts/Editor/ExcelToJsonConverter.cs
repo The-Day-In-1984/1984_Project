8c07e5630b3dc1aaef7fff84bc665197f31b2e68
Nakuri <ccmuk58@naver.com>
2023-04-10 19:22:47 +0900

    Refector: ExcelToJsonConverter Class

--- 1984/Assets/Scripts/Editor/ExcelToJsonConverter.cs
+++ 1984/Assets/Scripts/Editor/ExcelToJsonConverter.cs
@@ -1,10 +1,13 @@
 using Aspose.Cells;
 using UnityEditor;
-public class ExcelToJsonConverter
+public static class ExcelToJsonConverter
 {
     private const string DialogueExcelPath = "Assets/Resources/Dialogue/Dialogue.xlsx";
     private const string DialogueJsonPath = "Assets/Resources/Dialogue/DialogueJson.json";
     
+ 
+    /// <param name="inputPath">Assets/.../file.xlsx</param>
+    /// <param name="outputPath">Assets/.../file.json</param>
     private static void Convert(string inputPath, string outputPath)
     {
         var workbook = new Workbook(inputPath);
