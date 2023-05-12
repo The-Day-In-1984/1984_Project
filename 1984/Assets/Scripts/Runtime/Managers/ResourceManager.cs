b5741e64c4f3ae3717ff0407522bac9af48fcd5b
Jeongan Lee <84510455+fkdl0048@users.noreply.github.com>
2023-05-13 01:45:36 +0900

    Feat: Add ResourceManager

--- 1984/Assets/Scripts/Runtime/Managers/ResourceManager.cs
+++ 1984/Assets/Scripts/Runtime/Managers/ResourceManager.cs
@@ -4,5 +4,58 @@ using UnityEngine;
 
 public class ResourceManager
 {
-    
+    private readonly Dictionary<string, GameObject> _prefabs = new Dictionary<string, GameObject>();
+    private readonly Dictionary<string, Sprite> _sprites = new Dictionary<string, Sprite>();
+    private readonly Dictionary<string, AudioClip> _sounds = new Dictionary<string, AudioClip>();
+
+    private const string PrefabPath = "Prefabs/";
+    private const string SpritePath = "Sprites/";
+    private const string SoundPath = "Sounds/";
+    public GameObject LoadPrefab(string path)
+    {
+        if (_prefabs.ContainsKey(path))
+            return _prefabs[path];
+
+        GameObject prefab = Resources.Load<GameObject>(PrefabPath + path);
+        if (prefab == null)
+        {
+            Debug.LogError($"[ResourceManager] LoadPrefab Error: {path}");
+            return null;
+        }
+
+        _prefabs.Add(path, prefab);
+        return prefab;
+    }
+
+    public Sprite LoadSprite(string path)
+    {
+        if (_sprites.ContainsKey(path))
+            return _sprites[path];
+
+        Sprite sprite = Resources.Load<Sprite>(SpritePath + path);
+        if (sprite == null)
+        {
+            Debug.LogError($"[ResourceManager] LoadSprite Error: {path}");
+            return null;
+        }
+
+        _sprites.Add(path, sprite);
+        return sprite;
+    }
+
+    public AudioClip LoadSound(string path)
+    {
+        if (_sounds.ContainsKey(path))
+            return _sounds[path];
+
+        AudioClip sound = Resources.Load<AudioClip>(SoundPath + path);
+        if (sound == null)
+        {
+            Debug.LogError($"[ResourceManager] LoadSound Error: {path}");
+            return null;
+        }
+
+        _sounds.Add(path, sound);
+        return sound;
+    }
 }
