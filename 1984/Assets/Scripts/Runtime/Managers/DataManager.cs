883c994cb5c2446657a7c94d20219f3029f95693
Nakuri <ccmuk58@naver.com>
2023-05-12 10:52:02 +0900

    Fix: Excel sheet merge

--- 1984/Assets/Scripts/Runtime/Managers/DataManager.cs
+++ 1984/Assets/Scripts/Runtime/Managers/DataManager.cs
@@ -2,14 +2,8 @@ using System.Collections.Generic;
 using Structs;
 public class DataManager
 {
-    public Dictionary<string, List<StoryData.DialogueData>> DialogueData { get; private set; }
-    public Dictionary<string, List<StoryData.OptionData>> OptionData { get; private set; }
-    public Dictionary<string, List<StoryData.CharacterData>> CharacterData { get; private set; }
-    public Dictionary<string, List<StoryData.SceneData>> SceneData { get; private set; }
-    private readonly JsonDataConverter<StoryData.DialogueData> _dialogueDataConverter = new JsonDataConverter<StoryData.DialogueData>();
-    private readonly JsonDataConverter<StoryData.OptionData> _optionDataConverter = new JsonDataConverter<StoryData.OptionData>();
-    private readonly JsonDataConverter<StoryData.CharacterData> _characterDataConverter = new JsonDataConverter<StoryData.CharacterData>();
-    private readonly JsonDataConverter<StoryData.SceneData> _sceneDataConverter = new JsonDataConverter<StoryData.SceneData>();
+    public Dictionary<string, List<StoryData>> StoryData { get; private set; }
+    private readonly JsonDataConverter<StoryData> _storyDataConverter = new JsonDataConverter<StoryData>();
 
     public void Init()
     {
@@ -18,9 +12,6 @@ public class DataManager
 
     private void LoadStoryData()
     {
-        DialogueData = _dialogueDataConverter.GetDictionariesFromJson(Const.Consts.ResourcesStoryJsonPath)["Dialogue"];
-        OptionData = _optionDataConverter.GetDictionariesFromJson(Const.Consts.ResourcesStoryJsonPath)["Option"];
-        CharacterData = _characterDataConverter.GetDictionariesFromJson(Const.Consts.ResourcesStoryJsonPath)["Character"];
-        SceneData = _sceneDataConverter.GetDictionariesFromJson(Const.Consts.ResourcesStoryJsonPath)["Scene"];
+        StoryData = _storyDataConverter.GetDictionaryFromJson(Const.Consts.ResourcesStoryJsonPath);
     }
 }
