86aa1a877ff8c1f8880ec138eb2ee996af98c12c
fkdl0048 <fkdl000048@gmail.com>
2023-05-24 14:33:57 +0900

    Feat: Prototype Temp

--- 1984/Assets/Scripts/Runtime/UI/Mission/MissionController/Base/MissionController.cs
+++ 1984/Assets/Scripts/Runtime/UI/Mission/MissionController/Base/MissionController.cs
@@ -24,35 +24,27 @@ public class MissionController : MonoBehaviour
 
     protected virtual void CheckMissionComplete()
     {
-        Debug.Log("성공 한번 체크");
+       
     }
     
     protected virtual void CheckMissionFail()
     {
-        Debug.Log("실패 한번 체크");
+       
     }
-
     
     protected void MissionComplete()
     {
-        // 미션 성공하면 로직
-        // 화면 사라지거나 애니메이션 등등처리
         GameManager.Data.SetReliability(10);
-        _view.Hide(); // temp 연출 넣기
-        //_view.Test();
+        _view.Hide();
         
-        Debug.Log("성공");
         IsComplete = true;
     }
     
     protected void MissionFail()
     {
-        // 미션 실패하면 로직
-        // 화면 사라지거나 애니메이션 등등처리
         GameManager.Data.SetReliability(-10);
         _view.Hide();
         
-        Debug.Log("실패");
         IsComplete = true;
     }
     
