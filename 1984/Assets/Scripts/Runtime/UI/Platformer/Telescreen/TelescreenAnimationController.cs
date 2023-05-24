4c62ffa5d3682d4063a3fe237f9a3e5a7d97c183
kyoonge <96893605+kyoonge@users.noreply.github.com>
2023-05-25 00:41:05 +0900

    Feat: Telescreen animation

---
+++ 1984/Assets/Scripts/Runtime/UI/Platformer/Telescreen/TelescreenAnimationController.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Enums;
+
+public class TelescreenAnimationController : MonoBehaviour
+{
+    private Animator animator;
+    private Telescreen telescreen;
+    private GameObject bigBrother;
+
+    private ITelescreen previousState; // 이전 상태 저장
+
+    private void Start()
+    {
+        animator = GetComponent<Animator>();
+        telescreen = GetComponent<Telescreen>();
+        bigBrother = GameObject.Find("BigBrother");
+
+        previousState = telescreen.currentState; // 초기 상태 설정
+        UpdateAnimation();
+    }
+
+    private void Update()
+    {
+        if (previousState != telescreen.currentState)
+        {
+            UpdateAnimation();
+        }
+    }
+
+    private void UpdateAnimation()
+    {
+        switch (telescreen.currentState)
+        {
+            case TeleScreen.ReadyState:
+                PlayReadyAnimation();
+                break;
+            case TeleScreen.OnState:
+                PlayOnAnimation();
+                break;
+            case TeleScreen.OffState:
+                PlayOffAnimation();
+                break;
+        }
+
+        previousState = telescreen.currentState; // 상태 업데이트
+    }
+
+    private void PlayReadyAnimation()
+    {
+        //Debug.Log("ReadyAnimation");
+    }
+
+    private void PlayOnAnimation()
+    {
+        //Debug.Log("OnAnimation");
+        bigBrother.SetActive(true);
+    }
+
+    private void PlayOffAnimation()
+    {
+        bigBrother.SetActive(false);
+        //Debug.Log("OffAnimation");
+    }
+}
