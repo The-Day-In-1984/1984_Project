c65d940a0f751de0c15e6003575304ec4fe0c8e7
Nakuri <ccmuk58@naver.com>
2023-07-08 03:55:50 +0900

    Refactor: DialogueView

--- 1984/Assets/TempNextScene.cs
+++ 1984/Assets/TempNextScene.cs
@@ -8,8 +8,6 @@ public class TempNextScene : MonoBehaviour
 {
     private void OnTriggerEnter2D(Collider2D col)
     {
-        // 잠시만 자리좀 빌리겠습니다..
-        GameManager.Data._curDialogueId = "1";
         GameManager.UI.ChangeState(GameState.Dialogue);
     }
 }
