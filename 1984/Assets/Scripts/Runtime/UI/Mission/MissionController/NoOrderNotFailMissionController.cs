decd40d4ed2d09d0320d2b8f59325632cfddd5be
fkdl0048 <fkdl000048@gmail.com>
2023-07-26 15:33:53 +0900

    Feat: Drain Mission Done

--- 1984/Assets/Scripts/Runtime/UI/Mission/MissionController/NoOrderNotFailMissionController.cs
+++ 1984/Assets/Scripts/Runtime/UI/Mission/MissionController/NoOrderNotFailMissionController.cs
@@ -21,7 +21,7 @@ public class NoOrderNotFailMissionController : MissionController
     protected override void CheckMissionComplete()
     {
         base.CheckMissionComplete();
-        
+
         for (var i = 0; i < _missions.Count; i++)
         {
             if (_missions[i].IsIgnore)
